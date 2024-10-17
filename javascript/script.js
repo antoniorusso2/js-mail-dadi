555088bef911b17eeff1b070422771791b4abd78
antoniorusso2 <russoantonio303@outlook.it>
2024-10-17 17:54:08 +0200

    ADD: alternativa solution with for cycle for the mail check

--- javascript/script.js
+++ javascript/script.js
@@ -3,13 +3,29 @@
 const userEmail = prompt('Inserisci qui la tua mail') //string || null
 const allowAccess = ['prova1@mail.com', 'prova2@mail.com', 'prova3@mail.com', 'prova4@mail.com']
 
-if (allowAccess.includes(userEmail)) {
-  console.log('accesso garantito') 
-}
-else{
-  console.log('accesso negato')
-}
+// if (allowAccess.includes(userEmail)) {
+//   console.log('accesso garantito');
+// }
+// else{
+//   console.log('accesso negato');
+// }
 
 // console.log(userEmail)
 
 
+//!SOLUZIONE 2
+//ciclo for
+// console.log(Boolean(accessCheck))
+let accessCheck ;
+
+for (i = 0; i < allowAccess.length; i++){
+  accessCheck = allowAccess.includes(userEmail); //boolean
+  console.log(accessCheck);
+}
+
+if(accessCheck === true){
+  console.log('Accesso garantito');
+}
+else{
+  alert('Accesso Negato')
+}
