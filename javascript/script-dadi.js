21031926e96f5680fe9407affb4aff87a6737de9
antoniorusso2 <russoantonio303@outlook.it>
2024-10-17 17:26:57 +0200

    ADD: - another js file for a different script - script for prompt asking user for email; - a list of emails that are allowed to access and response message

---
+++ javascript/script-dadi.js
@@ -0,0 +1,11 @@
+
+// 2. Gioco dei dadi
+
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+const nUser = Math.random() * 6;
+const nCPU = Math.random() * 6;
+
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte?
+// Se dobbiamo confrontare qualcosa che “cosa” ci serve?
--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,15 @@
+// 1. Mail
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+const userEmail = prompt('Inserisci qui la tua mail') //string || null
+const allowAccess = ['prova1@mail.com', 'prova2@mail.com', 'prova3@mail.com', 'prova4@mail.com']
+
+if (allowAccess.includes(userEmail)) {
+  console.log('accesso garantito') 
+}
+else{
+  console.log('accesso negato')
+}
+
+// console.log(userEmail)
 
 
